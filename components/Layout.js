ef9daaf3a722c9372b9d9d4103190088acece4c1
sohagraha <sohagrahabd@gmail.com>
2022-04-02 12:26:55 +0600

    fix style issue when reload

--- components/Layout.js
+++ components/Layout.js
@@ -1,26 +1,28 @@
 import Head from 'next/head'
 import React from 'react'
 import { AppBar, Container, Toolbar, Typography } from '@mui/material';
+import useStyles from '../utils/styles'
 
 export default function Layout({ children }) {
+    const classes = useStyles();
     return (
         <div>
             <Head>
                 <title>ECOM BD</title>
             </Head>
-            <AppBar position="static">
+            <AppBar position="static" className={classes.navbar}>
                 <Toolbar>
                     <Typography>
                         ecom-bd
                     </Typography>
                 </Toolbar>
             </AppBar>
-            <Container>
+            <Container className={classes.main}>
                 {children}
             </Container>
-            <footer>
+            <footer className={classes.footer}>
                 <Typography>
-                    All right reserved sohag
+                    All right reserved ecom-bd
                 </Typography>
             </footer>
         </div>
---
+++ pages/_document.js
@@ -0,0 +1,36 @@
+import { ServerStyleSheets } from "@mui/styles";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import React from 'react';
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html land='eng'>
+                <Head>
+                    <body>
+                        <Main />
+                        <NextScript />
+                    </body>
+                </Head>
+            </Html>
+        )
+    }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+    ctx.renderPage = () => {
+        return originalRenderPage({
+            enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+        });
+    };
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+        ...initialProps,
+        styles: [
+            ...React.Children.toArray(initialProps.styles),
+            sheets.getStyleElement(),
+        ],
+    };
+};
